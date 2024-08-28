fa0289619e0736da04003e47e79207a5ba1b8756
Adiallo123 <aissatoudiallobd@gmail.com>
2024-08-28 17:46:15 +0200

    :bug: insert the data on the tables announce and company and change company_size and company-name

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -3,9 +3,9 @@ create table company (
   id int unsigned PRIMARY KEY AUTO_INCREMENT NOT NULL,
   email VARCHAR(155) not NULL,
   password VARCHAR(255) not NULL,
-  company_name VARCHAR(155) NOT NULL,
+  name VARCHAR(155) NOT NULL,
   phone VARCHAR(20) NOT NULL,
-  company_size INT NOT NULL,
+  size INT NOT NULL,
   validate BOOLEAN NOT NULL
 );
 
@@ -22,3 +22,14 @@ create table announce (
   company_id int unsigned not NULL,
   Foreign key (company_id) REFERENCES company(id)
   );
+
+  INSERT INTO company(email, password, name, phone, size, validate)
+  VALUES("auchan@auchan.fr", "0000", "auchan", "03087965412", 500, 1 ),
+  ("lidl@lidl.fr", "1234", "lidl", "03087965412", 300, 1 ),
+  ("action@action.fr", "azerty", "action", "03087965412", 100, 0 );
+
+  INSERT INTO announce ( job_title,  location, description, min_salary, max_salary, benefits, job_type, telework, company_id)
+VALUES('Agent de maîtrise', 'Saint-dié-des-Vosges', 'Tu maîtrises un peu tout on te demandera donc des choses divers et variés sans jamais attribuer de poste fixe', 1600, 1750, 'chèques cadeau', 'CDI', 'Présentiel', 1),
+('developpeur web', 'lille', 'Tu maîtrises un peu tout, on te demandera donc des choses divers et variés sans jamais attribuer de poste fixe', 1600, 1750, 'ticket resto', 'CDD', 'remote', 2),
+('Caissier', 'Bastia', 'Tu passeras des clients avec le sourire et un sens du service client parfait', 1500, 1600, 'chèques cadeau', 'CDD', 'Présentiel', 3);
+
