7297c54b1f48f93d8e5e4c88c822d8fdf4616271
Adiallo123 <aissatoudiallobd@gmail.com>
2024-08-28 15:34:32 +0200

    :sparkles: create table annonces and entreprise in schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,27 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+-- SQLBook: Code
+
+
+create table entreprise (
+  id int unsigned PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  email VARCHAR(155) not NULL,
+  password VARCHAR(255) not NULL,
+  description_annonces TEXT not NULL,
+  nom_entreprise VARCHAR(155) NOT NULL,
+  telephone VARCHAR(20) NOT NULL,
+  taille_entreprise INT NOT NULL,
+  validation BOOLEAN NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
-);
+create table annonces (
+  id int unsigned PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  intituler_de_poste VARCHAR(155) not NULL,
+  localisation VARCHAR(155) NULL,
+  description TEXT not NULL,
+  fourchette_de_salaire DECIMAL(10.2) not NULL,
+  avantages TEXT not NULL,
+  type_emploi VARCHAR(55) not NULL,
+  lieu_du_poste VARCHAR(55) not NULL,
+  entreprise_id int unsigned not NULL,
+  Foreign key (entreprise_id) REFERENCES entreprise(id)
+  );
+  
