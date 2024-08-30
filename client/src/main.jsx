9aa7d856ef8a97511df745690a432909ab8455b5
Pyriac <cyriacpineau@gmail.com>
2024-08-30 21:39:53 +0200

    :sparkles: switch case for delete working, still need to find error on put

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
 
 import myAxios from "./services/myAxios";
 
@@ -19,6 +23,33 @@ const router = createBrowserRouter([
           const reponse = await myAxios.get(`/api/announce/${params.id}`);
 
           return reponse.data;
+        },
+        action: async ({ request, params }) => {
+          const formData = await request.formData();
+          console.info(formData);
+          switch (request.method.toLowerCase()) {
+            case "put": {
+              await myAxios.put(`/api/announce/${params.id}`, {
+                job_title: formData.get("job_title"),
+                location: formData.get("location"),
+                description: formData.get("description"),
+                min_salary: formData.get("min_salary"),
+                max_salary: formData.get("max_salary"),
+                benefits: formData.get("benefits"),
+                job_type: formData.get("form_data"),
+                telework: formData.get("telework"),
+                id: params.id,
+              });
+              return redirect(`/`);
+            }
+            case "delete": {
+              await myAxios.delete(`/api/announce/${params.id}`);
+              return redirect(`/`);
+            }
+
+            default:
+              throw new Response("", { status: 405 });
+          }
         },
       },
     ],
