5b7f513c91590a5540877066b5f15cb68d9d13fc
Pyriac <cyriacpineau@gmail.com>
2024-09-01 09:33:49 +0200

    externalize loader and action logic in services page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  redirect,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import myAxios from "./services/myAxios";
+import announceIdLoader from "./services/announceIdLoader";
+import announceEditAction from "./services/announceEditAction";
 
 import App from "./App";
 import EditAnnounce from "./pages/EditAnnounce";
@@ -19,36 +16,8 @@ const router = createBrowserRouter([
       {
         path: "announce/:id/edit",
         element: <EditAnnounce />,
-        loader: async ({ params }) => {
-          const reponse = await myAxios.get(`/api/announce/${params.id}`);
-
-          return reponse.data;
-        },
-        action: async ({ request, params }) => {
-          const formData = await request.formData();
-          switch (request.method.toLowerCase()) {
-            case "put": {
-              await myAxios.put(`/api/announce/${params.id}`, {
-                job_title: formData.get("job_title"),
-                location: formData.get("location"),
-                description: formData.get("description"),
-                min_salary: formData.get("min_salary"),
-                max_salary: formData.get("max_salary"),
-                benefits: formData.get("benefits"),
-                job_type: formData.get("job_type"),
-                telework: formData.get("telework"),
-              });
-              return redirect(`/`);
-            }
-            case "delete": {
-              await myAxios.delete(`/api/announce/${params.id}`);
-              return redirect(`/`);
-            }
-
-            default:
-              throw new Response("", { status: 405 });
-          }
-        },
+        loader: announceIdLoader,
+        action: announceEditAction,
       },
     ],
   },
