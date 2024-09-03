f026783a10921dbe47c9733512624014773397b6
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-09-03 18:08:58 +0200

    :art: header background and picture done in homepage.jsx, added root in app.css and use it in homepage.css

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import HeaderPeopleExternatech from "../assets/images/Header_People_Externatech.png";
+
 export default function HomePage() {
   return (
     <>
@@ -7,21 +9,25 @@ export default function HomePage() {
         </h1>
       </div>
       <div className="HomePage_header_picture">
-        <p>
-          Avec toi jusqu'à
-          <br />
-          <span className="HomePage_span_white">ton premier jour</span> en
-          entreprise !
-        </p>
-        <div>
-          <button type="button">Cree ton profil en 3 clics</button>
-          <button type="button">Je recrute</button>
+        <div className="HomePage_phrase_button">
+          <p>
+            Avec toi jusqu'à
+            <br />
+            <span className="HomePage_span_white">ton premier jour</span> en
+            entreprise !
+          </p>
+          <div className="HomePage_header_button">
+            <button type="button">Cree ton profil en 3 clics</button>
+            <button type="button">Je recrute</button>
+          </div>
         </div>
+        <img src={HeaderPeopleExternatech} alt="three happy people" />
       </div>
       <div>
         <div>
           <h2>
-            Ces entreprises n'attendent que <span>vous</span> !
+            Ces entreprises n'attendent que{" "}
+            <span className="HomePage_span_green">vous</span> !
           </h2>
           <p>carrousel previous entreprises</p>
           <button type="button">Examiner plus d'entreprises</button>
