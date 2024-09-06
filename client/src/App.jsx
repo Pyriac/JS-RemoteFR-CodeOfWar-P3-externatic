86df9f8f3f5e87776da2505d681c69da3b8c618f
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-09-06 10:43:54 +0200

    :sparkles: import new package carrousel , add new features in carrouselCompanyhomepage.css add new function in carrouselcompanyhomepage components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,14 @@ import { Outlet } from "react-router-dom";
 import "./assets/styles/App.css";
 import "./assets/styles/EditAnnounce.css";
 import "./assets/styles/Homepage.css";
-
+import "./assets/styles/CarrouselCompanyHomePage.css";
 
 function App() {
-
   return (
-        <main>
-            <Outlet />
-        </main>
-  )
-
+    <main>
+      <Outlet />
+    </main>
+  );
 }
 
 export default App;
