0a8e4787616046767c2e13879a7066105b40a77c
samih ringuet <samrin2@proton.me>
2024-09-11 17:00:22 +0200

    :art: Created new component AnnounceDetailCompanyMore and deplaced code on it

--- client/src/components/AnnounceDetailCompanyDescription.jsx
+++ client/src/components/AnnounceDetailCompanyDescription.jsx
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 export default function Announcethirdbox({ announce }) {
   return (
    <div className="CompanyDetail_description">
-        <ul key={announce.id}>
+        <div key={announce.id}>
           <h2 className="CompanyDetail_title">L'entreprise</h2>
-          <li>Capgemini est un leader mondial dans le domaine des services de conseil, de transformation numérique, de technologie et d'ingénierie. Fondée en 1967 et présente dans plus de 50 pays, l'entreprise compte plus de 350 000 collaborateurs. Capgemini aide les plus grandes organisations à se transformer en combinant une expertise approfondie du secteur avec des solutions innovantes.</li>
+          <p>Capgemini est un leader mondial dans le domaine des services de conseil, de transformation numérique, de technologie et d'ingénierie. Fondée en 1967 et présente dans plus de 50 pays, l'entreprise compte plus de 350 000 collaborateurs. Capgemini aide les plus grandes organisations à se transformer en combinant une expertise approfondie du secteur avec des solutions innovantes.</p>
 
-        </ul>
+        </div>
     </div>
   );
 }
--- client/src/pages/AnnounceDetail.jsx
+++ client/src/pages/AnnounceDetail.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from 'react-router-dom';
 import AnnounceDetailTitle from "../components/AnnounceDetailTitle";
 import AnnounceDetailPost from "../components/AnnouncePostDetail";
 import AnnounceDetailCompanyDescription from "../components/AnnounceDetailCompanyDescription";
+import AnnounceDetailCompanyMore from "../components/AnnounceDetailCompanyMore";
 
 export default function AnnounceDetail() {
   const announce = useLoaderData(); 
@@ -15,19 +16,9 @@ export default function AnnounceDetail() {
 
       <AnnounceDetailCompanyDescription announce={announce}  />
 
-      <div className="AnnounceDetail_morecompanydescription">
-        <img src="path/to/your/image.jpg" alt="Illustration" className="cta-image" />
-        <h2>Vous voulez en savoir D'avantage sur Capgemini</h2>
-        <p>Et explorer leurs autres offres</p>
-        <div className="AnnounceDetail_morecompanydescription_button">
-          <button className="AnnounceDetail_discover_button" type="button">
-            Découvrir l'entreprise
-          </button>
-          <button className="AnnounceDetail_morecompanydescription_spontanée_button" type="button">
-            Candidature spontanée
-          </button>
-        </div>
-      </div>
+      <AnnounceDetailCompanyMore announce={announce}  />
+
+
     </div>
   );
 }
