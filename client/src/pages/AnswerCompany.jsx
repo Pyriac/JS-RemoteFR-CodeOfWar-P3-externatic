20eb37707d70bc51851c7c00ac2593a35460c5fa
Adiallo123 <aissatoudiallobd@gmail.com>
2024-09-27 17:46:19 +0200

    :lipstick: add css on answerCompany

--- client/src/pages/AnswerCompany.jsx
+++ client/src/pages/AnswerCompany.jsx
@@ -2,21 +2,22 @@ import { useLoaderData } from "react-router-dom";
 
 function AnswerCompany() {
   const myAnswers = useLoaderData();
-  console.info(myAnswers);
   return (
     <div className="Company_Container">
       <h2>Mes retour pour cette offre{`(${myAnswers.length})`}</h2>
-      {myAnswers.map((myAnswer) => (
-        <ul key={myAnswer.id} className="Card_company">
-          <li>{myAnswer.title}</li>
-          <li>{myAnswer.first_name}</li>
-          <li>{myAnswer.last_name}</li>
-          <li>{myAnswer.email}</li>
-          <li>{myAnswer.location}</li>
-          <li>{myAnswer.phone}</li>
-          <li>{myAnswer.cv}</li>
-        </ul>
-      ))}
+      <div className="Company_Contain">
+        {myAnswers.map((myAnswer) => (
+          <ul key={myAnswer.id} className="Card_Company">
+            <li>{myAnswer.title}</li>
+            <li>{myAnswer.first_name}</li>
+            <li>{myAnswer.last_name}</li>
+            <li>{myAnswer.email}</li>
+            <li>📍{myAnswer.location}</li>
+            <li>{myAnswer.phone}</li>
+            <li>{myAnswer.cv}</li>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
